0e5b4de259cef09fec3c8747a3b5b84520d35c59
blacknii <42908977+blacknii@users.noreply.github.com>
2023-07-19 19:49:57 +0200

    .

--- src/components/main-content/Types.tsx
+++ src/components/main-content/Types.tsx
@@ -7,6 +7,28 @@ interface TypesProps {
   setPage: (newTypes: number) => void;
 }
 
+type ColorType = {
+  backgroundColor?: string;
+  color?: string;
+  "&:hover"?: { backgroundColor: string };
+  border: string;
+  borderColor: string;
+};
+
+type TypeData =
+  | {
+      name: string;
+      color: string;
+      iconWhite: undefined;
+      iconColored: undefined;
+    }
+  | {
+      name: string;
+      color: string;
+      iconWhite: JSX.Element;
+      iconColored: JSX.Element;
+    };
+
 const Types = ({ selectedTypes, setSelectedTypes, setPage }: TypesProps) => {
   const handleTypes = (newtype: string) => {
     if (newtype === "all types") {
@@ -27,6 +49,41 @@ const Types = ({ selectedTypes, setSelectedTypes, setPage }: TypesProps) => {
     setPage(1);
   };
 
+  const getColorAndIcon = (type: TypeData, selectedTypes: string[]) => {
+    let color: ColorType = {
+      border: "solid 1px",
+      borderColor: "pokemonTypes.chip.outline",
+    };
+    let icon;
+
+    if (selectedTypes.includes(type.name)) {
+      color = {
+        ...color,
+        backgroundColor: `pokemonTypes.${type.name}.main`,
+        color: `pokemonTypes.${type.name}.contrastText`,
+        "&:hover": { backgroundColor: `pokemonTypes.${type.name}.light` },
+      };
+      icon = true;
+    } else if (selectedTypes.length === 0 && type.name === "all types") {
+      color = {
+        ...color,
+        backgroundColor: `primary.main`,
+        color: `pokemonTypes.chip.contrastText`,
+        "&:hover": { backgroundColor: `primary.light` },
+      };
+      icon = true;
+    } else {
+      color = {
+        ...color,
+        backgroundColor: "pokemonTypes.chip.fill",
+        "&:hover": { backgroundColor: `primary` },
+      };
+      icon = false;
+    }
+
+    return { color, icon };
+  };
+
   return (
     <Box
       sx={{
@@ -37,36 +94,7 @@ const Types = ({ selectedTypes, setSelectedTypes, setPage }: TypesProps) => {
       }}
     >
       {types.map((type) => {
-        let color;
-        let icon;
-        if (selectedTypes.includes(type.name)) {
-          color = {
-            backgroundColor: `pokemonTypes.${type.name}.main`,
-            color: `pokemonTypes.${type.name}.contrastText`,
-            border: "solid 1px",
-            borderColor: "pokemonTypes.chip.outline",
-            "&:hover": { backgroundColor: `pokemonTypes.${type.name}.light` },
-          };
-          icon = true;
-        } else if (selectedTypes.length === 0 && type.name === "all types") {
-          color = {
-            backgroundColor: `primary.main`,
-            color: `pokemonTypes.chip.contrastText`,
-            border: "solid 1px",
-            borderColor: "pokemonTypes.chip.outline",
-            "&:hover": { backgroundColor: `primary.light` },
-          };
-          icon = true;
-        } else {
-          color = {
-            border: "solid 1px",
-            borderColor: "pokemonTypes.chip.outline",
-            backgroundColor: "pokemonTypes.chip.fill",
-            "&:hover": { backgroundColor: `primary` },
-          };
-          icon = false;
-        }
-
+        const { color, icon } = getColorAndIcon(type, selectedTypes);
         return (
           <Chip
             icon={icon ? type.iconWhite : type.iconColored}
